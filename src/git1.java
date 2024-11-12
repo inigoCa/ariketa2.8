a5c15d910beb055a6ffb49ded46528dbd8432f70
ik012982i12 <icalvolo24wg@ikzubirimanteo.com>
2024-11-12 13:52:07 +0100

    batuketa gehitzen

---
+++ src/git1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class git1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("eman balore bat");
+        int a = sc.nextInt();
+        System.out.println("eman beste balore bat");
+        int b = sc.nextInt();
+        int batuketa = a+b;
+        System.out.println(batuketa);
+        int kenketa = a-b;
+        System.out.println(kenketa);
+
+        
+    }
+}
